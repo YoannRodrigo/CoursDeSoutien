7bdbbc377851c78b514c1a6aefe7b249bf9066d9
Yoann <yomaisonneuve@gmail.com>
2019-12-03 00:54:22 +0100

    Lesson 1.1

---
+++ CoursDeSoutien/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Animator animator;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Faire bouger le joueur
+        //Utiliser la bibliothèque Input
+        //Appliquer une force sur le rigibody
+    }
+}
