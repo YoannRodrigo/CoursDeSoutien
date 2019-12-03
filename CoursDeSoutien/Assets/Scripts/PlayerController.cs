caea5ef66ba69cee54a9c29c735f6abb9f700277
Yoann <yomaisonneuve@gmail.com>
2019-12-03 23:06:50 +0100

    One solution for player movement

--- CoursDeSoutien/Assets/Scripts/PlayerController.cs
+++ CoursDeSoutien/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animator animator;
+    public float speed;
+    public float rotationSpeed = 200.0f;
+    private float moveHorizontal;
+    private float moveVertical;
+    private Vector3 movement;
+    
+    private Vector3 newRotation;
     
     private void Start()
     {
@@ -18,8 +25,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //Faire bouger le joueur
-        //Utiliser la bibliothèque Input
-        //Appliquer une force sur le rigibody
+        bool isRunning = false;
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(speed * Time.deltaTime * transform.forward,0);
+            isRunning = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-speed * Time.deltaTime* transform.forward,0);
+            isRunning = true;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(-speed * Time.deltaTime* transform.right,0);
+            isRunning = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime* transform.right,0);
+            isRunning = true;
+        }
+        animator.SetBool("IsRunning", isRunning);
     }
 }
