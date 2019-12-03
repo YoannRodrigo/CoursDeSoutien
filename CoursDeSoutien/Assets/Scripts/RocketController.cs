ea6b15254baeb85db98a659d4a3e96ea5d8bb521
Yoann <yomaisonneuve@gmail.com>
2019-12-03 11:27:20 +0100

    Lesson 1.2

---
+++ CoursDeSoutien/Assets/Scripts/RocketController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RocketController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private const float FORCE_VALUE = 2f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Turret"))
+        {
+            if (other.CompareTag("Player"))
+            {
+                Destroy(other.gameObject);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(FORCE_VALUE*transform.forward, ForceMode.Impulse);
+    }
+}
---
+++ CoursDeSoutien/Assets/Scripts/TurretControler.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SphereCollider))]
+public class TurretControler : MonoBehaviour
+{
+    public string tagPlayer;
+    public GameObject missilePrefab;
+    //Faire une liste des points de départs des missiles
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //Si c'est le joueur, il faut que la tourelle pivote vers lui et tire six fois
+    }
+    
+    
+}
