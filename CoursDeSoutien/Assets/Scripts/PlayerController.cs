5b4066b1f644c8df64dc14f1600b1824636c19df
Louis <louis.leclair6@mail.com>
2019-12-04 17:58:42 +0100

    C'est mon camion
    *klaxon de gros lard*

--- CoursDeSoutien/Assets/Scripts/PlayerController.cs
+++ CoursDeSoutien/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animator))] // si le script n'a pas ses composants, il les recrée ou vous empêche de les supprimer
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animator animator;
+    private const float SPEED = 4;
     
     private void Start()
     {
@@ -19,7 +20,29 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         //Faire bouger le joueur
-        //Utiliser la bibliothèque Input
-        //Appliquer une force sur le rigibody
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(SPEED * transform.forward * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(SPEED * -transform.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(SPEED * -transform.right * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(SPEED * transform.right * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(SPEED * transform.up * Time.deltaTime);
+        }
     }
 }
