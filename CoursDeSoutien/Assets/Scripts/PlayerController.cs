48301b442640d85770bedde5884cb9bf787c0e27
Al <->
2019-12-11 19:16:29 +0100

    Le personnage a maintenant une animation !

--- CoursDeSoutien/Assets/Scripts/PlayerController.cs
+++ CoursDeSoutien/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animator animator;
+    private Animation run;
     public float speed = 0.5f;
     public float jumpForce = 0.5f;
     
@@ -20,29 +21,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        bool isRunning = false;
         if(Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(speed * Time.deltaTime * transform.forward, 0);
+            isRunning = true;
         }
 
         if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(speed * Time.deltaTime * - transform.right, 0);
+            isRunning = true;
         }
 
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(speed * Time.deltaTime * transform.right, 0);
+            isRunning = true;
         }
 
         if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(speed * Time.deltaTime * - transform.forward, 0);
+            isRunning = true;
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        animator.SetBool("IsRunning", isRunning);
     }
 }
